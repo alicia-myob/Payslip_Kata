af3b269a270be9574251abda242ca450389016a4
Alicia Shen <Alicia.Shen@c02y30gajg5j.myob.myobcorp.net>
2020-11-20 11:31:00 +1300

    [001][feat] Setting up main class and VS console

---
+++ Main.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collection.Generics;
+
+namespace Payslip{
+    
+    class Main{
+        
+        private Dictionary<string, string> _info = new Dictionary<string, string>();
+
+        public static void Main(String[] args){
+            Console.WriteLine("Welcome to the payslip generator! \n");
+            
+
+        }
+
+        public bool validator(string input){
+            return false;
+        }
+
+    }
+}
